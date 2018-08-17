8fc1412c393c48e42c75660a825f6233098b6f17
Sarah Manning <sarahnmanning@gmail.com>
2018-08-17 18:48:20 -0400

    initial commit. set up prelininary dependencies in json pkg and set up skeleton of server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const axios = require('axios');
+
+const server = express();
+
+//port
+const port  = precess.env.PORT || 5005;
+
+//environment
+
+
+//middleware
+
+
+//errorhandling
+
+
+//404 handler
+
+
+//on stage
+server.listen(port () =>{
+    console.log(`Now listening on port: ${port}`);
+});
