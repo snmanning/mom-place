de3907bb7b3b8ef0ddc01fe9cae437fee5f4964e
Sarah Manning <sarahnmanning@gmail.com>
2018-08-31 11:49:39 -0400

    link to login page

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
 class Signup extends Component {
     render() {
@@ -10,6 +11,13 @@ class Signup extends Component {
                     <div><input type='email' placeholder='Email' /></div>
                     <div><input type='password' placeholder='Password' /></div>
                     <div><button type='submit'>Sign me up!</button></div>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to='/login'>Back to Login</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </form>
             </div>
         );
